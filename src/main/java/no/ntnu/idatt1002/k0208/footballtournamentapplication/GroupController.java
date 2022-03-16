97012ad71b941c6f25659a65260346764f20269c
Kasia Szlejter <kasia.szlejter@gmail.com>
2022-03-16 11:04:29 +0100

    Added some basic functionality to GroupController

--- src/main/java/no/ntnu/idatt1002/k0208/footballtournamentapplication/GroupController.java
+++ src/main/java/no/ntnu/idatt1002/k0208/footballtournamentapplication/GroupController.java
@@ -1,4 +1,33 @@
 package no.ntnu.idatt1002.k0208.footballtournamentapplication;
 
+import java.util.ArrayList;
+
+/**
+ * GroupController class that stores football clubs and generates groups.
+ */
 public class GroupController {
+    private ArrayList<FootballClub> footballClubs;
+
+    /**
+     * GroupController constructor
+     */
+    public GroupController() {
+        this.footballClubs = new ArrayList<>(64);
+    }
+
+    public ArrayList<FootballClub> getFootballClubs() {
+        return footballClubs;
+    }
+
+    public void addFootballClub(String name, String nationality) {
+        FootballClub team = new FootballClub(name, nationality);
+        this.footballClubs.add(team);
+    }
+
+    @Override
+    public String toString() {
+        return "GroupController{" +
+                "footballClubs=" + footballClubs +
+                '}';
+    }
 }
