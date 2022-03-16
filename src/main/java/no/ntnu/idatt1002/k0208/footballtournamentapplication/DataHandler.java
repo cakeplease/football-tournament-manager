08fa6b32f0c5893093aef291db14db6cb6a4e293
Kasia Szlejter <kasia.szlejter@gmail.com>
2022-03-16 12:46:39 +0100

    Added DataHandler class

---
+++ src/main/java/no/ntnu/idatt1002/k0208/footballtournamentapplication/DataHandler.java
@@ -0,0 +1,9 @@
+package no.ntnu.idatt1002.k0208.footballtournamentapplication;
+
+/**
+ * Model part of the system.
+ * Takes care of all the data handling and storing
+ */
+public class DataHandler {
+
+}
