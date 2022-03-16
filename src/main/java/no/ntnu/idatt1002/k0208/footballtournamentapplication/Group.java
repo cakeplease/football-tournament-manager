2360a2dac913453b3f19c405738a822c4cf7a580
Ann-Marie <ann.marie.amr@gmail.com>
2022-03-16 09:27:11 +0100

    spelling fix 2

--- src/main/java/no/ntnu/idatt1002/k0208/footballtournamentapplication/Group.java
+++ src/main/java/no/ntnu/idatt1002/k0208/footballtournamentapplication/Group.java
@@ -3,51 +3,51 @@ package no.ntnu.idatt1002.k0208.footballtournamentapplication;
 import java.util.ArrayList;
 
 /**
- * Class for controlling groupe play
+ * Class for controlling group play
  *
  * @author birkn
  * @version 1.01 15.03.2022
  */
 public class Group {
-    private final ArrayList<FootballClub> groupeTeams;
-    private ArrayList<Match> groupeMatches = null;
+    private final ArrayList<FootballClub> groupTeams;
+    private ArrayList<Match> groupMatches = null;
 
     /**
-     * constructor for getting predetermined groupe
-     * assigns groupeTeams to existing arraylist
-     * @param groupeTeams existing arraylist of type fotbalclub
+     * constructor for getting predetermined group
+     * assigns groupTeams to existing arraylist
+     * @param groupTeams existing arraylist of type fotbalclub
      * @throws IllegalArgumentException if arraylist size larger then 4 or if array is null
      */
-    public Group(ArrayList<FootballClub> groupeTeams) throws IllegalArgumentException{
-        if (groupeTeams == null) {
-            throw new IllegalArgumentException("grope teams cant be null");
+    public Group(ArrayList<FootballClub> groupTeams) throws IllegalArgumentException{
+        if (groupTeams == null) {
+            throw new IllegalArgumentException("group teams cant be null");
         } else
-        if (groupeTeams.size() > 4){
-            throw new IllegalArgumentException("Groupe size larger then allowed");
+        if (groupTeams.size() > 4){
+            throw new IllegalArgumentException("Group size larger then allowed");
         }
-        this.groupeTeams = groupeTeams;
+        this.groupTeams = groupTeams;
     }
 
     /**
-     * overload constructer if groupe is not determined beforhand
+     * overload constructer if group is not determined beforhand
      */
     public Group(){
-        this.groupeTeams = new ArrayList<FootballClub>();
+        this.groupTeams = new ArrayList<FootballClub>();
     }
 
     /**
-     * adds team to groupe
-     * @param teamToAdd team to add to groupe
+     * adds team to group
+     * @param teamToAdd team to add to group
      * @return boolean if team is added or not
      * @throws IndexOutOfBoundsException if arraysize >= 4
      */
     public boolean addTeam(FootballClub teamToAdd) throws IndexOutOfBoundsException{
-        if (groupeTeams.size() >= 4)
-            throw new IndexOutOfBoundsException("Groupes cant have more then 4 teams");
-        if(this.groupeTeams.contains(teamToAdd))
+        if (groupTeams.size() >= 4)
+            throw new IndexOutOfBoundsException("Groups cant have more then 4 teams");
+        if(this.groupTeams.contains(teamToAdd))
             return false;
 
-        this.groupeTeams.add(teamToAdd);
+        this.groupTeams.add(teamToAdd);
         return true;
     }
 
@@ -57,15 +57,15 @@ public class Group {
      * @throws Exception if group size != 4
      */
     public void genereateMatches() throws Exception {
-        if (this.groupeTeams.size() != 4){
-            throw new Exception("There needs to be 4 teams in the groupe");
-        }else if(this.groupeMatches == null){
-            this.groupeMatches = new ArrayList<Match>();
+        if (this.groupTeams.size() != 4){
+            throw new Exception("There needs to be 4 teams in the group");
+        }else if(this.groupMatches == null){
+            this.groupMatches = new ArrayList<Match>();
 
             //loops threw the array and adds all matches to match array
-            for (int i = 0; i < this.groupeTeams.size(); i++) {
-                for (int j = i + 1; j < this.groupeTeams.size(); j++) {
-                    this.groupeMatches.add(new Match(this.groupeTeams.get(i), this.groupeTeams.get(j), 0, 0));
+            for (int i = 0; i < this.groupTeams.size(); i++) {
+                for (int j = i + 1; j < this.groupTeams.size(); j++) {
+                    this.groupMatches.add(new Match(this.groupTeams.get(i), this.groupTeams.get(j), 0, 0));
                 }
             }
         }
