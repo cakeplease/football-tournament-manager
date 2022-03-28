a5579588a17b0128d1bd8cdefa811a485a3756e6
BIrk <>
2022-03-28 12:45:13 +0200

    added methods for loading and storing data, also creates file when saving

--- src/main/java/model/DataHandler.java
+++ src/main/java/model/DataHandler.java
@@ -1,6 +1,7 @@
 package model;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
@@ -21,7 +22,15 @@ public class DataHandler {
      * @param data String of data to save
      * @param path path, where to save it
      */
-    public static void saveToFile(String data, Path path) {
+    public static void saveToFile(String data, Path path) throws RuntimeException{
+        File file = new File(String.valueOf(path));
+        if (!file.exists()) {
+            try {
+                file.createNewFile();
+            }catch (IOException e){
+                throw new RuntimeException("file coud not be created");
+            }
+        }
         try {
             Files.writeString(path, data, StandardCharsets.UTF_8);
         } catch (IOException e) {
@@ -43,7 +52,6 @@ public class DataHandler {
             }
             return data;
         } catch (IOException e) {
-            e.printStackTrace();
             return null;
         }
     }
--- src/main/java/model/DataStorage.java
+++ src/main/java/model/DataStorage.java
@@ -1,44 +1,52 @@
 package model;
 
 import base.FootballClub;
+import base.Group;
 import controller.GroupController;
-import javafx.beans.DefaultProperty;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Objects;
+import java.util.stream.Collectors;
 
+/**
+ * class to save and load to and from file
+ * @author birkn, vebjørn
+ * @version 28.03.2022
+ */
 public class DataStorage {
     private static final String footballClubsPath = "src/main/resources/data/FootballClubs.csv";
     private static final String footballClubsTestPath = "src/main/resources/data/FootballClubsTest.csv";
+    private static final String groupPath = "src/main/resources/data/GroupData.csv";
+    private static final String groupPathTest = "src/main/resources/data/GroupDataTest.csv";
     private static final String COMMA_DELIMITER = ",";
 
 
     /**
      * Method for saving football clubs to csv-file
      * Loops through the football clubs and adds the data to the cvs-file (path)
+     * @param test boolean to not overwrite savefile
+     *
      */
     public static void saveFootballClubs(boolean test) {
         StringBuilder data = new StringBuilder();
         for (FootballClub fc : GroupController.getInstance().getFootballClubs()) {
-            data.append(fc.getCsvFormat());
+            data.append(fc.getCsvFormat() + "\n");
         }
         DataHandler.saveToFile(data.toString(), getFootballClubsPath(test));
     }
 
     /**
      * reads footballclubs from file, and creates array of them
+     * @param test boolean to not overwrite savefile
      * @throws RuntimeException if footballclubs coud not be read from file
      */
-    public static void loadFootballClubsFromFile(boolean test)throws RuntimeException{
+    private static void loadFootballClubsFromFile(boolean test)throws RuntimeException{
         try {
             ArrayList<FootballClub> footballClubsFromFIle = new ArrayList<FootballClub>();
-            Objects.requireNonNull(DataHandler.readFromFile(getFootballClubsPath(test))).forEach(e -> {
-                String[] values = e.split(COMMA_DELIMITER);
-                footballClubsFromFIle.add(new FootballClub(values[0],values[1], Integer.parseInt(values[2]),
-                        Integer.parseInt(values[3]), Integer.parseInt(values[4])));
-            });
+            Objects.requireNonNull(DataHandler.readFromFile(getFootballClubsPath(test))).forEach(e ->
+                    footballClubsFromFIle.add(parseFootballClub(e)));
 
             GroupController.getInstance().addAll(footballClubsFromFIle);
         } catch (NullPointerException e){
@@ -49,10 +57,77 @@ public class DataStorage {
     /**
      * returns the path for file reading later or for tests
      * as path
+     * @param test if test or not (to not overide savefile
      * @return footballclubs path
      */
-    public static Path getFootballClubsPath(boolean test){
+    private static Path getFootballClubsPath(boolean test){
         return Paths.get((test) ? footballClubsTestPath : footballClubsPath);
+    }
+
+    private static Path getGroupPath(boolean test){
+        return Paths.get((test) ? groupPathTest : groupPath);
+    }
+
+    private static void saveGroupToFile(boolean test){
+        DataHandler.saveToFile(GroupController.getInstance().getGroups().stream().map(Group::generateCsv)
+                .collect(Collectors.joining("")), getGroupPath(test));
+
+    }
+
+    /**
+     * loades grupes from file
+     * @throws RuntimeException if gupeSaveFile coud not be read
+     */
+    private static void loadGrupes(boolean test) throws RuntimeException{
+        try {
+            ArrayList<FootballClub> footballClubs = GroupController.getInstance().getFootballClubs();
+            Objects.requireNonNull(DataHandler.readFromFile(getGroupPath(test))).forEach(e -> {
+                String[] data = e.split(";");
+                Group tempGroup = new Group();
+                tempGroup.setGroupNumber(Integer.parseInt(data[0]));
+                for (int i = 1; i < data.length; i++) {
+
+                    //this is to get the references right, and not just deep copy
+                    int index = footballClubs.indexOf(parseFootballClub(data[i]));
+                    if (index != -1)
+                        tempGroup.addTeam(footballClubs.get(index));
+                }
+                GroupController.getInstance().getGroups().add(tempGroup);
+            });
+        }catch (NullPointerException e){
+            throw new RuntimeException("cant read data from file");
+        }
+    }
+
+    /**
+     * creates football club from csv string
+     * @param csvString to parse
+     * @return Footballclub
+     */
+    private static FootballClub parseFootballClub(String csvString){
+        String[] values = csvString.split(COMMA_DELIMITER);
+        return new FootballClub(values[0],values[1], Integer.parseInt(values[2]),
+                Integer.parseInt(values[3]), Integer.parseInt(values[4]));
+    }
+
+
+    /**
+     * saves all data
+     * @param test if test
+     */
+    public static void save(boolean test){
+        saveFootballClubs(test);
+        saveGroupToFile(test);
+    }
+
+    /**
+     * loades all data
+     * @param test if test
+     */
+    public static void load(boolean test){
+        GroupController.getInstance().resetList();
+        loadFootballClubsFromFile(test);
+        loadGrupes(test);
     }
 
 
